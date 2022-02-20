351eed4c9f340680277148e0bf8af7c34541efb6
JohnLkt <john.lukito@gmail.com>
2022-02-20 18:52:12 +0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,44 @@
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("typetask").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("Space Cannot Be Empty!");
+    } else {
+      document.getElementById("tasklist").appendChild(li);
+    }
+    document.getElementById("typetask").value = "";
+  
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
